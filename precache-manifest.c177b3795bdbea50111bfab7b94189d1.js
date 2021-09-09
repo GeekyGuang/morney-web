73f58d9e0a1e2ffa5544ac417a45336884cd7e52
travis <travis.x.wong@gmail.com>
2021-09-09 21:30:45 +0800

    deploy

---
+++ precache-manifest.c177b3795bdbea50111bfab7b94189d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0a5a3c380910152e7a",
+    "url": "/morney-web/css/app.03fddf09.css"
+  },
+  {
+    "revision": "8c4d24828abbc249c91c2acaaa428f0b",
+    "url": "/morney-web/index.html"
+  },
+  {
+    "revision": "9a0a5a3c380910152e7a",
+    "url": "/morney-web/js/app.2c368e9a.js"
+  },
+  {
+    "revision": "98b81748c6f3222e4d90",
+    "url": "/morney-web/js/chunk-vendors.06b0e8cb.js"
+  },
+  {
+    "revision": "3150727a70a9e016a1ec83088e86ce7f",
+    "url": "/morney-web/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-web/robots.txt"
+  }
+]);
